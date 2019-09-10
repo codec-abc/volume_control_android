81e50be87bd2d7b1f9f146c7d29069e019e625f2
cviot <viot.camille@outlook.com>
2019-09-10 19:52:48 +0200

    stuff

--- app/src/main/java/com/example/punksta/volumecontrol/EventActivity.java
+++ app/src/main/java/com/example/punksta/volumecontrol/EventActivity.java
@@ -1,14 +1,44 @@
 package com.example.punksta.volumecontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import androidx.fragment.app.FragmentTransaction;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class EventActivity extends AppCompatActivity {
+import com.example.punksta.volumecontrol.view.EventListAndAddFragment;
+
+public class EventActivity extends AppCompatActivity implements EventListAndAddFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+        //getSupportActionBar().setTitle(R.string.save); // TODO
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Fragment f1 = new EventListAndAddFragment();
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        ft.replace(R.id.eventActivityFrame, f1); // f1_container is your FrameLayout container
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        //ft.addToBackStack(null); // TODO
+        ft.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return true;
     }
 }
